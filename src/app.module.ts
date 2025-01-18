be3e01b17f1f2644c447fbc0a0202c7b4e031e4e
tlthiago <thiagoluizmachadoalves@gmail.com>
2025-01-17 21:30:00 -0300

    feat: adds first endpoint `/status`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './config/database/database.module';
+import { StatusModule } from './modules/status/status.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.development.local',
+    }),
+    DatabaseModule,
+    StatusModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
