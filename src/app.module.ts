70b02a933dbfa1febab52e4e7fc5d658c05149ce
Paulo Gonçalves <65259656+phg007@users.noreply.github.com>
2025-01-30 18:40:48 -0300

    refactor(employees): update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { CostCentersModule } from './modules/cost-centers/cost-centers.module';
 import { CbosModule } from './modules/cbos/cbos.module';
 import { EpisModule } from './modules/epis/epis.module';
 import { EmployeesModule } from './modules/employees/employees.module';
-import { ProjectsModule } from './modules/projects/projects.module'
+import { ProjectsModule } from './modules/projects/projects.module';
 
 @Module({
   imports: [
