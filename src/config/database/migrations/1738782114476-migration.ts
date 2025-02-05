6e641d81c076246a0f752e0d43538a3df5cda23e
phg 007 <paulo.henriqueicm2011@gmail.com>
2025-02-05 16:06:52 -0300

    feat(employees): upating API

--- src/config/database/migrations/1738782114476-migration.ts
+++ src/config/database/migrations/1738782114476-migration.ts
@@ -1,16 +1,23 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1738782114476 implements MigrationInterface {
-    name = 'Migration1738782114476'
+  name = 'Migration1738782114476';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "funcionarios" ADD "projetosId" integer`);
-        await queryRunner.query(`ALTER TABLE "funcionarios" ADD CONSTRAINT "FK_d68bd8f53ce5209781947bae972" FOREIGN KEY ("projetosId") REFERENCES "projetos"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "funcionarios" ADD "projetosId" integer`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "funcionarios" ADD CONSTRAINT "FK_d68bd8f53ce5209781947bae972" FOREIGN KEY ("projetosId") REFERENCES "projetos"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "funcionarios" DROP CONSTRAINT "FK_d68bd8f53ce5209781947bae972"`);
-        await queryRunner.query(`ALTER TABLE "funcionarios" DROP COLUMN "projetosId"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "funcionarios" DROP CONSTRAINT "FK_d68bd8f53ce5209781947bae972"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "funcionarios" DROP COLUMN "projetosId"`,
+    );
+  }
 }
