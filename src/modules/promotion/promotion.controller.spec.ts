449f4a477f9b814b41927fae9076984519d68573
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2025-06-12 10:30:51 -0300

    feat: adds `empresas/empresaId/promocoes` endpoint and updateEmployeeRole method

--- src/modules/promotion/promotion.controller.spec.ts
+++ src/modules/promotion/promotion.controller.spec.ts
@@ -45,7 +45,9 @@ describe('PromotionController (E2E)', () => {
   let dataSource: DataSource;
   let mockUserInterceptor: MockUserInterceptor;
   let createdUser: User;
+  let createdRole: Role;
   let createdEmployee: Employee;
+  let createdCompany: Company;
 
   const promotion = {
     funcaoId: '1',
@@ -136,12 +138,12 @@ describe('PromotionController (E2E)', () => {
       email: 'contato@techsolutions.com.br',
       celular: '+5531991897926',
     });
-    const createdCompany = await companyRepository.save(company);
+    createdCompany = await companyRepository.save(company);
 
     const role = roleRepository.create({
       nome: 'Função Teste',
     });
-    const createdRole = await roleRepository.save(role);
+    createdRole = await roleRepository.save(role);
     promotion.funcaoId = createdRole.id;
 
     const department = departmentRepository.create({
@@ -204,17 +206,35 @@ describe('PromotionController (E2E)', () => {
     }
   });
 
-  it('/v1/funcionarios/:funcionarioId/promocoes (POST) - Deve cadastrar uma promoção', async () => {
+  it('/v1/funcionarios/:funcionarioId/promocoes (POST) - Deve retornar erro ao promover o funcionário para a mesma função que ele já possui', async () => {
     const response = await request(app.getHttpServer())
       .post(`/v1/funcionarios/${createdEmployee.id}/promocoes`)
-      .send(promotion)
+      .send({ ...promotion })
+      .expect(409);
+
+    expect(response.body).toEqual({
+      statusCode: 409,
+      message: 'O funcionário já possui essa função.',
+      error: 'Conflict',
+    });
+  });
+
+  it('/v1/funcionarios/:funcionarioId/promocoes (POST) - Deve cadastrar uma promoção', async () => {
+    const roleRepository = dataSource.getRepository(Role);
+    const createdRole = await roleRepository.save({
+      nome: 'Função Teste 2',
+    });
+
+    const response = await request(app.getHttpServer())
+      .post(`/v1/funcionarios/${createdEmployee.id}/promocoes`)
+      .send({ ...promotion, funcaoId: createdRole.id })
       .expect(201);
 
     expect(response.status).toBe(201);
     expect(response.body).toMatchObject({
       succeeded: true,
       data: {
-        salario: promotion.salario,
+        salario: '3799.00',
       },
       message: expect.stringContaining(
         'Promoção cadastrada com sucesso, id: #',
@@ -291,6 +311,35 @@ describe('PromotionController (E2E)', () => {
     });
   });
 
+  it('/v1/empresas/:empresaId/promocoes (GET) - Deve listar todas as promoções de uma empresa', async () => {
+    const promotionRepository = dataSource.getRepository(Promotion);
+    await promotionRepository.save({
+      ...promotion,
+      funcao: createdRole,
+      funcionario: createdEmployee,
+      criadoPor: createdUser,
+    });
+
+    const response = await request(app.getHttpServer())
+      .get(`/v1/empresas/${createdCompany.id}/promocoes`)
+      .expect(200);
+
+    expect(response.body).toBeInstanceOf(Array);
+    expect(response.body.length).toBeGreaterThan(0);
+  });
+
+  it('/v1/empresas/:empresaId/promocoes (GET) - Deve retornar erro caso o ID da empresa não exista', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/v1/empresas/86f226c4-38b0-464c-987e-35293033faf6/promocoes`)
+      .expect(404);
+
+    expect(response.body).toEqual({
+      statusCode: 404,
+      message: 'Empresa não encontrada.',
+      error: 'Not Found',
+    });
+  });
+
   it('/v1/funcionarios/:funcionarioId/promocoes (GET) - Deve listar todas as promoções de um funcionário', async () => {
     const promotionRepository = dataSource.getRepository(Promotion);
     await promotionRepository.save({
@@ -304,6 +353,18 @@ describe('PromotionController (E2E)', () => {
 
     expect(response.body).toBeInstanceOf(Array);
     expect(response.body.length).toBeGreaterThan(0);
+  });
+
+  it('/v1/funcionarios/:funcionarioId/promocoes (GET) - Deve retornar erro caso o ID do funcionário não exista', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/v1/funcionarios/86f226c4-38b0-464c-987e-35293033faf6/promocoes`)
+      .expect(404);
+
+    expect(response.body).toEqual({
+      statusCode: 404,
+      message: 'Funcionário não encontrado.',
+      error: 'Not Found',
+    });
   });
 
   it('/v1/funcionarios/promocoes/:id (GET) - Deve retonar uma promoção específica', async () => {
