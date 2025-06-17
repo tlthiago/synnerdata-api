4d8703006756cae8ea6fc267f68b5a25761e19f1
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2025-06-17 12:10:42 -0300

    feat: adds companies updated migration file

--- src/config/database/migrations/1750096059520-migration.ts
+++ src/config/database/migrations/1750096059520-migration.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1750096059520 implements MigrationInterface {
-    name = 'Migration1750096059520'
+  name = 'Migration1750096059520';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "empresas" ADD "id_assinatura" character varying`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "empresas" ADD "id_assinatura" character varying`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "empresas" DROP COLUMN "id_assinatura"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "empresas" DROP COLUMN "id_assinatura"`,
+    );
+  }
 }
